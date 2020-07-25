96c74d64e40b1c0fe725fa1116acce303d67f768
timothy <timothyabiodun300@gmail.com>
2020-07-25 23:31:59 +0100

    Managing Data with sqlite

---
+++ app/src/main/java/com/example/notekeeper/NoteKeeperOpenHelper.java
@@ -0,0 +1,26 @@
+package com.example.notekeeper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class NoteKeeperOpenHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "NoteKeeper.db";
+    public static final int DATABASE_VERSION = 1;
+    public NoteKeeperOpenHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(NoteKeeperDatabaseContract.CourseInfoEntry.SQL_CREATE_TABLE);
+        sqLiteDatabase.execSQL(NoteKeeperDatabaseContract.NoteInfoEntry.SQL_CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
